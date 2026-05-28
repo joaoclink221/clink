c231dde18bbacc7ca2aa62ca18b75ad7ff5cc2a3
joaoclink221 <joaoclink222@gmail.com>
2026-05-28 03:10:47 -0300

    feat: função incluir clientes criada

--- src/clink.java
+++ src/clink.java
@@ -45,6 +45,57 @@ public class clink {
             }
         }
         return novoCliente;
+     } // Essa função vai servir para aumentar o tamanho da matriz de clientes, adicionando uma nova linha.
+
+     public static String [][] incluirCliente(String[][] clientes, Scanner sc){
+        int maiorCodigo = 0;
+
+        boolean cheio =true;
+
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i][0] == null) {
+                cheio = false;
+                break;
+            }
+        }
+        if (cheio) {
+            clientes = aumentarMatrizClientes(clientes);
+        }
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i][0] != null) {
+                int codigo = Integer.parseInt(clientes[i][0]);
+                if (codigo>maiorCodigo) maiorCodigo = codigo;
+            }
+        }
+        int novoCodigo = maiorCodigo+1;
+
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i][0] == null) {
+                clientes[i][0] = String.valueOf(novoCodigo);
+                System.out.println("informe o nome:");
+                clientes[i][1] = sc.nextLine();
+
+                System.out.println("informe o CPF ou CNPJ:");
+                clientes[i][2] = sc.nextLine();
+
+                System.out.println("informe o Data de nascimento:");
+                clientes[i][3] = sc.nextLine();
+
+                System.out.println("informe o Sexo:");
+                clientes[i][4] = sc.nextLine();
+
+                System.out.println("informe o Cidade:");
+                clientes[i][5] = sc.nextLine();
+
+                System.out.println("informe o Estado:");
+                clientes[i][6] = sc.nextLine();
+
+                System.out.println("informe o Status:");
+                clientes[i][7] = sc.nextLine();
+                break;
+            }
+        }
+       return clientes;
      }
 
     /* =========  MAIN  ========= */
